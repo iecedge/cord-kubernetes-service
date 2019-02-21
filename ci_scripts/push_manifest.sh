21ea96d02b900f7b4db9d0b05861c5b60d4b88dc
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-02-21 17:25:03 -0400

    Move to new Cachengo infra

--- ci_scripts/push_manifest.sh
+++ ci_scripts/push_manifest.sh
@@ -1,4 +1,5 @@
 export IMAGE_TAG=$(cat VERSION)
+export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker manifest create --amend cachengo/kubernetes-synchronizer:$IMAGE_TAG cachengo/kubernetes-synchronizer-x86_64:$IMAGE_TAG cachengo/kubernetes-synchronizer-aarch64:$IMAGE_TAG
 docker manifest push cachengo/kubernetes-synchronizer:$IMAGE_TAG
